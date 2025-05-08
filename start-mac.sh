0b64a8e780a613144c468440e55b9cecc78365ce
Jon Udell <jon@jonudell.info>
2025-05-08 13:20:20 -0700

    fix name

--- start-mac.sh
+++ start-mac.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./xmlui-test-server-macos-arm -api sql/api.json -show-responses 2>&1 | tee server_log.txt
+./xmlui-test-server-mac-arm -api sql/api.json -show-responses 2>&1 | tee server_log.txt
