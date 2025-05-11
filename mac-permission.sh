5d8c8a99d629cce944f19cde39bc4277355011b1
Jon Udell <jon@jonudell.info>
2025-05-11 10:32:15 -0700

    redo start scripts

--- mac-permission.sh
+++
@@ -1,2 +0,0 @@
-chmod +x tiny-httpd-mac-arm && \
-xattr -d com.apple.quarantine tiny-httpd-mac-arm 2>/dev/null || true
--- start-mac.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./xmlui-test-server-mac-arm -api sql/api.json -show-responses 2>&1 | tee server_log.txt
