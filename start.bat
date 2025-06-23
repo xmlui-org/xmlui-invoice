61f51064d7502bc2fc1e0d5ae3bc8ae7bff1640c
Jon Udell <jon@jonudell.info>
2025-06-22 19:12:05 -0700

    adjust build

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 @echo off
 setlocal
 
-REM Run the server with the API configuration
-test-server\xmlui-test-server-windows-amd64.exe -api api.json
+REM Change to app directory and run server with secure file paths
+cd xmlui-invoice
+..\test-server\xmlui-test-server-windows-amd64.exe -db ..\private\data.db -api ..\private\api.json
