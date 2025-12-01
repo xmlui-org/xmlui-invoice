28dc008c1f08793e17b4f8d3d8ca5aebe1fd8403
Norbert Hajagos <hajagosnorbi@gmail.com>
2025-12-01 15:01:49 +0100

    use 'cat' instead of 100 echoes

--- start.sh
+++ start.sh
@@ -3,19 +3,22 @@ set -e
 
 # Check if xmlui is in the path
 if ! command -v xmlui &> /dev/null; then
-    echo ""
-    echo "'xmlui' not found in PATH."
-    echo ""
-    echo "xmlui-invoice demonstrates a full-stack application."
-    echo "It relies on a server that talks to a local database."
-    echo "The 'xmlui' CLI does exaclty that."
-    echo ""
-    echo "Please ensure the 'xmlui' CLI is installed and available in your system PATH."
-    echo "Download the self-contaied executable from https://xmlui.org/download/xmlui-cli"
-    echo ""
-    echo "If you don't want to do that, but you can already start"
-    echo "a local http server, check out our other demo applications"
-    echo "at https://xmlui.org/download/apps"
+    cat <<EOF
+
+'xmlui' not found in PATH.
+
+xmlui-invoice demonstrates a full-stack application.
+It relies on a server that talks to a local database.
+The 'xmlui' CLI does exaclty that.
+
+Please ensure the 'xmlui' CLI is installed and available in your system PATH.
+Download the self-contaied executable from https://xmlui.org/download/xmlui-cli
+
+If you don't want to do that, but you can already start
+a local http server, check out our other demo applications
+at https://xmlui.org/download/apps
+
+EOF
     exit 1
 fi
 
